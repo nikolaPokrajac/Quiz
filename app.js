5f38130a31900e539a824dac3af18ce5857bd1ad
nikolaPokrajac <nikolapokrajac92@gmail.com>
2019-06-09 00:25:05 +0200

    Result animation.

--- app.js
+++ app.js
@@ -22,6 +22,15 @@ form.addEventListener("submit", e => {
 
   // Show result on page
   scrollTo(0, 0);
-  result.querySelector("span").textContent = `${score}%`;
   result.classList.remove("d-none");
+
+  let output = 0;
+  const timer = setInterval(() => {
+    result.querySelector("span").textContent = `${output}%`;
+    if (output === score) {
+      clearInterval(timer);
+    } else {
+      output++;
+    }
+  }, 10);
 });
